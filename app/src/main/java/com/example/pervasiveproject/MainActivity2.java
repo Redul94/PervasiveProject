5bc41f04a7d8619eb4be801639caf9a196a14317
SIFAT11 <sifat15-3402@diu.edu.bd>
2023-05-15 11:52:46 +0600

    sectry

--- app/src/main/java/com/example/pervasiveproject/MainActivity2.java
+++ app/src/main/java/com/example/pervasiveproject/MainActivity2.java
@@ -2,20 +2,50 @@ package com.example.pervasiveproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity  {
     Spinner spinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         spinner=findViewById(R.id.spinnerId);
-        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array., android.R.layout.simple_spinner_item);
+
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.spinner, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (id==1) {
+                    Intent intent = new Intent(getApplicationContext(), BasicCalActivity3.class);
+                    startActivity(intent);
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.i("A","NOthing selected");
+
+
+            }
+        });
     }
+
+
+
+
 }
